e16b224c413564403aaff42e7c91d9187268e544
Venetrius <gergelygjuhasz@gmail.com>
2019-02-23 12:56:39 -0700

    Nav items navigation works

--- src/App.js
+++ src/App.js
@@ -2,66 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function getNavItem(dest, name, link ){
-  if (dest == name){
-    return(
-      <li class="nav-item active">
-        <a class="nav-link" href="#">{name} <span class="sr-only">(current)</span></a>
-    </li>
-    );
-  }else{
-    return(
-      <li class="nav-item">
-        <a class="nav-link" href={link}>{name} <span class="sr-only">(current)</span></a>
+
+
+class  Header extends React.Component{
+
+  renderNavItem(name){
+    if (this.props.page === name){
+      return(
+        <li class="nav-item active">
+          <a class="nav-link"> {name}</a> 
       </li>
-    );
+      );
+    }else{
+      return(
+        <li class="nav-item">
+          <a class="nav-link" onClick={() =>{this.props.onClick(name)}}> {name}</a>
+        </li>
+      );
+    }
   }
-}
 
-function getHeader(active){
+  render(){
   return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#" > <img src={logo} alt="logo"></img>  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-                                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#" > <img src={logo} alt="logo"></img>  </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      {getNavItem({active}, "Dynamic", "https://www.google.com")}
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
-                                                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onClick="">Search</button>
-    </form>
-  </div>
-</nav>
-  );
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          {this.renderNavItem("Dynamic")}
+          {this.renderNavItem("home")}
+          {this.renderNavItem("about")}
+          {this.renderNavItem("contact")}
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onClick="">Search</button>
+        </form>
+      </div>
+    </nav>
+  )
+  }
 }
 
 function getFooter(){
@@ -110,11 +94,45 @@ function getFooter(){
 
 class App extends Component {
 
+  constructor(props) {
+    super (props);
+    this.state = {
+      page: "home"
+    };
+  }
+
+  handleClick(nextPage) {
+    this.setState({
+      page: nextPage,
+    });
+  }
+
+  /* TODO test funcionality remove */
+  inPage(currPage, pageName) {
+      if(currPage === pageName){
+        return(
+          <label>The two page name is identical</label>
+        )
+      }else{
+        return(
+          <label>The two page name is not identical</label>
+        )
+      }
+    
+  }
+
   render() {
     return (
       <div>
-      {getHeader("home")}
+      <Header page = {this.state.page} onClick={(i) => this.handleClick(i) } />
       <br></br>
+      <button onClick={() => {this.handleClick("Dynamic")}} >Go Dynamic {this.state.page}</button>
+      <button onClick={() => {this.handleClick("home")}} >Go home {this.state.page}</button>
+      {/* TODO test funcionality remove */}
+      <div>State bar <br/>
+                <span>state : {this.state.page}</span>
+                {<span> {this.inPage(this.state.page, "home")} </span>}
+      </div>
       {getFooter()}
       </div>
     );
