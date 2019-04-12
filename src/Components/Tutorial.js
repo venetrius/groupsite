e65bc07fa25a92ce186c4716a5167b9fcda507ee
venetrius <gergelygjuhasz@gmail.com>
2019-04-11 22:30:26 -0600

    rewrite tutorials page content

--- src/Components/Tutorial.js
+++ src/Components/Tutorial.js
@@ -25,7 +25,7 @@ class Tutorial extends React.Component {
           <div class="col wSidebar">
             <h1>Tutorial</h1>
             <br></br>
-            <h2> If you are looking for a way to cach up to the group this might help you</h2>
+            <h2> If you are looking for a way to catch up to the group this might help you</h2>
             <br></br><br></br>
             <p id="why">
               <h3>Why do we learn React.js?</h3>
@@ -44,34 +44,89 @@ class Tutorial extends React.Component {
             <p id="tutorial">List of tutorials
                     <ul>
                 <li><a target="_blank" href="https://reactjs.org/tutorial/tutorial.html">
-                  'Official' React turorial hands on (Picking up terminology on the way)
+                  'Official' React tutorial hands-on (Picking up terminology on the way)
                             </a></li>
-                <li><a target="_blank" href="https://www.robinwieruch.de/javascript-fundamentals-react-requirements/#react-arrow-functions">A tutorial with more fundamentals, but without excercises</a></li>
+                <li><a target="_blank" href="https://www.robinwieruch.de/javascript-fundamentals-react-requirements/#react-arrow-functions">A tutorial with more fundamentals, but without exercises</a></li>
               </ul>
             </p>
 
-            <h4>There are a few things you should definetly know (from prev. experience / tutorials) to
-                be able to join or sessions.
+            <h4>While we are happy to see you on any of our hackathons, there are a few things you should definitely know (from prev. experience / tutorials) to
+                get the best out of our sessions.
                 </h4>
             <br />
             <p id="JSX">
               <h4>Javascript XML (JSX)</h4>
-              {example("It provides the ability to write JS code as XML document, it translated into HTML before rendering it in the browser"
-                , `function foo() {
-  return (<div>bar</div>) 
+              {example(
+                <div>
+                  &nbsp;&nbsp; It provides the ability to write JS code as an XML document, which will be translated into HTML before rendering it to the browser.
+                  In other words, it lets you refer React Components as HTML tags.
+                  <br/>&nbsp;&nbsp;In this example, Foo is a class which  extends React.Component. What is a React.Component?
+                  See the next entry...
+                  Note: &nbsp; <q>Fundamentally, JSX just provides syntactic sugar for the React.createElement(component, props, ...children) function.</q>
+                  <br/>One more thing. When a function returns a JSX code snippet it should be enclosed into one parent tag.
+                </div>
+              , `class Foo extends React.Component {
+  render(){                
+    return (<div>bar</div>) 
+  }
 }
 {/*....*/}
 <div>
-  <foo></foo>
+  <Foo/>
 </div>`                  )}
             </p>
-            <p><h4>React Components</h4>
-              {example("It provides the ability to write JS code as XML document, which will be translated into HTML before rendering it to the browser"
-                , `function foo() {
+
+            <br/>
+            <p id="tutorial-component"> <h4>React Components</h4>
+              {example(
+                <div>A class which extends React.Component has a handful of useful features 
+                  <ul>
+                    <li>A React component has a state, which can be set in the cunstructor. Later on, you can modify a state of a component 
+                        by calling the this.setstate() function. Doing that will (sooner or later) result of the rerendering of the component. 
+                        For more check out a &nbsp;<a href="https://reactjs.org/docs/react-component.html#setstate">useful article</a>.
+                    </li>
+                    <li>
+                      The render method is the only mandatory method what you have to implement. It is automatically called when your component gets used.
+                      It should return the appropriate JSX code. 
+                    </li>
+                    <li>
+                      Other useful methods are&nbsp;
+                       <a href="https://reactjs.org/docs/react-component.html#componentdidmount">componentDidMount()</a> 
+                      &nbsp;and&nbsp;<a href="https://reactjs.org/docs/react-component.html#componentDidUpdate()">componentDidUpdate()</a> &nbsp;
+                      but you do not have to know everything just to start to learn... 
+                    </li>
+                  </ul>
+                </div>
+                , 
+`class Foo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      someBool: true,
+      prop: props.val,
+      otherProp: proprs.otherVal
+        };
+  
+  methodExample(){
+    this.setState(
+      {
+        someBool : false
+      }
+    )
+  }
+        
+  render(){                
     return (<div>bar</div>) 
-}`                 )}
+  }
+}
+
+/*usage*/
+<Foo val=specificVal otherVal={this.functionCall()}/>
+`                 
+                )}
             </p>
-            <p id="tutorial-component">
+
+            <p id="tutorial-component2">
               <h4>Components and function</h4>
               <h5>In React you can create a lot of things</h5>
             </p>
@@ -81,11 +136,11 @@ class Tutorial extends React.Component {
   }
 }
 
-function example(title, toHighlight) {
+function example(description, toHighlight) {
   return (
     <div class="row row-container">
       <div class="col-12 col-md-6">
-        {title}
+        {description}
       </div>
       <div class="col-12 col-md-6">
         <Highlight language="javascript">{toHighlight}</Highlight>
