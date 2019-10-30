9ccad7da2c95aeb7a888a0dedb77487b0fabc6af
venetrius <gergelygjuhasz@gmail.com>
2019-10-29 21:25:28 -0600

    render project from db

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,105 +1,62 @@
 import React, { Component } from "react";
 import "./Projects.css";
-import ProjectList from './ProjectList';
-import AddProject from './AddProject';
+
 class Projects extends React.Component {
-    
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        list: []
-      };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      list: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("http://localhost:3030/projects")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  renderProject(project){
+    return (
+      <div key={project.id}>
+        <h4>{project.name}</h4>
+        <div>{project.stack.split(',').join(' ')}</div>
+        <p>{project.description}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      const list = items.map(project => this.renderProject(project))
+      return (
+        <div>
+          {list}
+        </div>
+      );
     }
-
-    handleAddProject=(project)=>{
-
-      if(!project){
-        return "Enter valid project";
-      }
-
-      else if(this.state.list.indexOf(project)>-1){
-        return "This project already exists.";
-      }
-
-      this.setState((prevState)=>({
-        list:prevState.list.concat(project)
-      }));
-    }
-
-    handleDeleteProject=(projectToDelete)=>{
-      this.setState((prevState)=>({
-        list:prevState.list.filter((project)=>project!==projectToDelete)
-      }));
-    }
-
-    handleDeleteProjects=()=>{
-      this.setState({
-        list:[]
-      });
-    }
-  
-    componentDidMount() {
-      console.log("project page is loading")
-      /*fetch("http://localhost:3030/projects")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              items: result.items
-            });
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-      */
-     this.setState({
-      isLoaded:true,
-      items : [{message : 'finish me'}]
-    })
-    }
-    componentWillUnmount(){
-      console.log("Goodbye!");
-    }
-
-
-    
-
-
-  
-    render() {
-      const { error, isLoaded, items } = this.state;
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <div>
-
-            <ProjectList
-            list={this.state.list}
-            handleDeleteProject={this.handleDeleteProject}
-            handleDeleteProjects={this.handleDeleteProjects}>
-              </ProjectList>
-            
-          
-          <AddProject handleAddProject={this.handleAddProject}/>
-          </div>
-        );
-        
-      }
-    }
-
+  }
 }
 
 export default Projects;
