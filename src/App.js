e8ba290158028caca628328b3f5c630713f16c7f
Simon <29654835+absolutelysimon@users.noreply.github.com>
2020-03-14 10:56:19 -0600

    Testing AWS.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Header, Footer, Body } from './Components';
+import React, { Component } from "react";
+import "./App.css";
+import { Header, Footer, Body } from "./Components";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pages: [
+                "Home",
+                "Tutorial",
+                "Events",
+                "Projects",
+                "Join us",
+                "Contact"
+            ],
+            page: "Home"
+        };
+    }
 
-  constructor(props) {
-    super (props);
-    this.state = {
-      pages: ["Home", "Tutorial", "Events", "Projects", "Join us", "Contact"],
-      page: "Home"
-    };
-  }
+    handleClick(nextPage) {
+        this.setState({
+            page: nextPage
+        });
+    }
 
-  handleClick(nextPage) {
-    this.setState({
-      page: nextPage,
-    });
-  }
+    render() {
+        return (
+            <div>
+                <Header
+                    page={this.state.page}
+                    onClick={i => this.handleClick(i)}
+                    pages={this.state.pages}
+                />
+                <div className="CCcontent">
+                    <Body
+                        page={this.state.page}
+                        onClick={i => this.handleClick(i)}
+                    />
+                </div>
 
-  render() {
-    return (
-
-      <div>
-      <Header page = {this.state.page} onClick={(i) => this.handleClick(i)} pages = {this.state.pages} />
-      <div className="CCcontent" >
-      <Body  page={this.state.page} onClick={(i) => this.handleClick(i)} />
-      </div>
-      
-      <Footer></Footer>
-      </div>
-    );
-  }
-
-  
+                <Footer></Footer>
+            </div>
+        );
+    }
 }
 
 export default App;
