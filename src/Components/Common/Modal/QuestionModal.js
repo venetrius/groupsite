ae920131a62f02b6e8bc20362ac094cf9a4a1808
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2020-05-15 10:22:46 -0600

    replace bootstrap to ant in QuestionModal component

--- src/Components/Common/Modal/QuestionModal.js
+++ src/Components/Common/Modal/QuestionModal.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Modal, Form, Input, Button } from 'antd';
 import './QuestionModal.css';
 
 
@@ -9,57 +7,55 @@ import './QuestionModal.css';
 class QuestionModal extends Component {
     constructor(props, context) {
       super(props, context);
-  
       this.handleShow = this.handleShow.bind(this);
       this.handleClose = this.handleClose.bind(this);
-  
+
       this.state = {
         show: false,
       };
     }
-  
+
     handleClose() {
       this.setState({ show: false });
     }
-  
+
     handleShow() {
       this.setState({ show: true });
     }
-  
+
     render() {
       return (
         <>
-          <Button variant="primary" onClick={this.handleShow}>
+          <Button type="primary" onClick={this.handleShow}>
             {this.props.buttonText}
           </Button>
-  
-          <Modal show={this.state.show} onHide={this.handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Ask a question</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form>
-              <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control required type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a valid city.
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Label>Question</Form.Label>
-                  <Form.Control as="textarea" rows="3" />
-                </Form.Group>
-                <Button className="modal-button" variant="secondary" onClick={this.handleClose}>
-                Close
-              </Button>
-                <Button className="modal-button" type="submit">Ask</Button>
-              </Form>
-            </Modal.Body>
-            
+
+          <Modal
+            visible={this.state.show}
+            onOk={this.handleClose}
+            onCancel={this.handleClose}
+            title="Ask a question"
+          >
+            <Form onFinish={this.handleClose}>
+              <Form.Item
+                label="Email address"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter a valid email adress",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Question"
+                name="question"
+              >
+                <Input.TextArea />
+              </Form.Item>
+            </Form>
           </Modal>
         </>
       );
