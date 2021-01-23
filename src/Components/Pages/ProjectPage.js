c3290acd51dc3f24dd81485944aec0127d9a6aab
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2021-01-23 08:53:17 -0700

    style / refactor projectPage WIP

--- src/Components/Pages/ProjectPage.js
+++ src/Components/Pages/ProjectPage.js
@@ -4,12 +4,16 @@ import { URL } from "../../config";
 import { Button, Comment, Avatar, Tooltip, Input } from "antd";
 import moment from "moment";
 import 'antd/dist/antd.css';
-
+import Tag from 'antd/es/tag'
 
 import { withRouter } from "react-router";
 import NewProject from '../Common/NewCommentForm'
 
 const { TextArea } = Input;
+
+const colors = [ 'pink', 'red', 'yellow', 'orange', 'cyan', 'green', 'blue', 'purple', 'geekblue', 'magenta', 'volcano', 'gold', 'lime']
+
+const getRandomVariant = () => colors[Math.floor(Math.random()*colors.length)]
 
 class ProjectPage extends React.Component {
   constructor(props) {
@@ -40,33 +44,16 @@ class ProjectPage extends React.Component {
       );
   }
 
-  ExampleComment = (comment) => (
-    <Comment
-      actions={[<span key="comment-nested-reply-to">Reply to</span>]}
-      author={<a>Han Solo</a>}
-      avatar={
-        <Button type="text" shape="circle">
-          AB
-      </Button>
-      }
-      content={
-        <p>
-          We supply a series of design principles, practical patterns and high
-          quality design resources (Sketch and Axure).
-        </p>
-      }
-      datetime={
-        <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
-          <span>{moment().fromNow()}</span>
-        </Tooltip>
-      }
-    >
-    </Comment>
-  );
+  renderTechStack = (techStackAsString) => (
+    <div>
+      {techStackAsString.split(',').map(tech =>
+        <Tag key={`${tech}-badge`} color={getRandomVariant()} style={{marginRight: "20px", float: 'right'}}> {tech} </Tag>
+      )}
+    </div>)
 
   Comment = (comment) => (
     <Comment
-      author={<a>comment.user_name</a>}
+      author={<a>{comment.user_name}</a>}
       avatar={
         <Button type="text" shape="circle">
           AB
@@ -79,62 +66,14 @@ class ProjectPage extends React.Component {
       }
       datetime={
         <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
-          <span>{moment().fromNow()}</span>
+          <span>{moment(comment.created_at).fromNow()}</span>
         </Tooltip>
       }
     >
     </Comment>
   );
 
-  dataField = (label, data) => <span></span>;
-
-  comp = () => {
-    return (
-      <div>
-        <h1> This is the project name</h1>
-        <p>
-          {" "}
-          This is a project to help people do something. Main goal is that the
-          thing with the stuff. Strech goal, do something else as well.
-        </p>
-        <span> Created at</span>
-        <span> 2020.11.12</span>
-        <span style={{ float: "right" }}>List of techs</span>
-        <div></div>
-        <div>
-          <h3>Description</h3>
-          <p>It can have multiple paragraphs</p>
-          <p>More about goals and plans</p>
-          <p>
-            Some details how much work has alredy been added. Maybe about the
-            group dinamics of the people who work on this stuff. Some link some
-            ideas. But mostly enough text for two lines.
-          </p>
-        </div>
-        <div>
-          <h3>Discussion</h3>
-          <div>
-            This is a place to ask your question, discover how many person is
-            interested in this idea etc. This is the default message what is
-            always (?) shown
-          </div>
-          <div>
-            This is a place to ask your question, discover how many person is
-            interested in this idea etc. This is the default message what is
-            always (?) shown
-          </div>
-          {this.ExampleComment()}
-          {this.ExampleComment()}
-          {this.ExampleComment()}
-        </div>
-        <div style={{ maxWidth: "80%", margin: "auto" }}>
-          <TextArea rows={4} />
-        </div>
-      </div>
-    );
-  };
-
-  handleAddProject(formData, page) {
+  handleAddComment(formData, page) {
     const projectId = page.props.match.params.projectId;
     axios.post(`${URL}/projects/${projectId}/comments`, formData)
     .then(response => {
@@ -155,18 +94,20 @@ class ProjectPage extends React.Component {
       return "Loading";
     }
     if (this.state.hasError) {
-      return this.comp();
       return "Opps some error, pls reload the page";
     }
     console.log(this.state);
     const { project, comments } = this.state;
     return (
-      <div>
+      <div style={{maxWidth: "1250px", margin: 'auto'}}>
+        <div>
         <h1> {project.name} </h1>
-        <p> {project.summary}</p>
+        <span style={{display: 'block', width: '100%', textAlign: 'center'}}>{project.summary}</span>
+        </div>
+
         <span> Created at</span>
         <span> {moment(project.created_at).format("YYYY/MM/DD")}</span>
-        <span style={{ float: "right" }}>List of techs</span>
+        {this.renderTechStack(project.selected_stack)}
         <div></div>
         <div>
           <h3>Description</h3>
@@ -179,13 +120,10 @@ class ProjectPage extends React.Component {
             interested in this idea etc. This is the default message what is
             always (?) shown
           </div>
-          {this.ExampleComment()}
-          {this.ExampleComment()}
-          {this.ExampleComment()}
         </div>
-        {this.state.comments.map(comment => this.Comment(comment))}
+        {comments.map(comment => this.Comment(comment))}
         <div style={{ maxWidth: "80%", margin: "auto" }}>
-          <NewProject onSubmit={this.handleAddProject} page={this}/>
+          <NewProject onSubmit={this.handleAddComment} page={this}/>
         </div>
 
       </div>
